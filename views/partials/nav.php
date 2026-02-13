10dda5c02499de5bcb5324c5796b22f35d8bb58d
Kris-2006 <88359324+Kris-2006@users.noreply.github.com>
2026-02-13 18:22:14 +0530

    if not what ?

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,10 +8,10 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-              <a href="/laracasts/demo/index.php" <?php echo $_SERVER['REQUEST_URI'] === '/laracasts/demo/index.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>Home</a>
-              <a href="/laracasts/demo/about.php" <?php echo $_SERVER['REQUEST_URI'] === '/laracasts/demo/about.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>About</a>
-              <a href="/laracasts/demo/contact.php" <?php echo $_SERVER['REQUEST_URI'] === '/laracasts/demo/contact.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>Contact</a>
-             <a href="/laracasts/demo/notes.php" <?php echo $_SERVER['REQUEST_URI'] === '/laracasts/demo/notes.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>Notes</a>
+              <a href="/index.php" <?php echo $_SERVER['REQUEST_URI'] === '/index.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>Home</a>
+              <a href="/about.php" <?php echo $_SERVER['REQUEST_URI'] === '/about.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>About</a>
+              <a href="/contact.php" <?php echo $_SERVER['REQUEST_URI'] === '/contact.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>Contact</a>
+             <a href="/notes.php" <?php echo $_SERVER['REQUEST_URI'] === '/notes.php' ? 'aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"' : 'class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"' ?>>Notes</a>
 
             
             </div>
